e24b012f4e5619a5bf00e1aebd4f2d3d2c4800d3
Mehmet Nazliay <mhmtnzly@gmail.com>
2022-08-15 15:59:31 +0200

    completedv4

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -28,11 +28,11 @@ class TriviaTestCase(unittest.TestCase):
                              "answer": "test1", "difficulty": "5", "category": "5"}
 
         # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
+        # with self.app.app_context():
+        #     self.db = SQLAlchemy()
+        #     self.db.init_app(self.app)
+        #     # create all tables
+        #     self.db.create_all()
 
     def tearDown(self):
         """Executed after reach test"""
@@ -50,7 +50,21 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["success"], True)
         self.assertTrue(data["total_questions"])
-        self.assertTrue(len(data["questions"]))
+
+    def test_get_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(len(data["categories"]), 6)
+
+    def test_404_invalid_get_categories(self):
+        res = self.client().get("/categories/1")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
 
     def test_404_sent_requesting_beyond_valid_page(self):
         res = self.client().get("/questions?page=250")
@@ -90,13 +104,13 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data["created"])
         self.assertTrue(len(data["questions"]))
 
-    def test_405_if_question_creation_not_allowed(self):
-        res = self.client().post("/questions/45", json=self.new_question)
+    def test_500_if_question_creation_not_allowed(self):
+        res = self.client().post("/questions")
         data = json.loads(res.data)
 
-        self.assertEqual(res.status_code, 405)
+        self.assertEqual(res.status_code, 500)
         self.assertEqual(data["success"], False)
-        self.assertEqual(data["message"], "method not allowed")
+        self.assertEqual(data["message"], "server errors")
 
     def test_get_question_search_with_results(self):
         res = self.client().post("/questions", json={"searchTerm": "title"})
@@ -107,7 +121,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data["total_questions"])
         self.assertEqual(len(data["questions"]), 2)
 
-    def test_get_question_search_without_results(self):
+    def test_get_book_search_without_results(self):
         res = self.client().post("/questions", json={"searchTerm": "how1"})
         data = json.loads(res.data)
 
@@ -147,6 +161,11 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data["success"], False)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
 
 
 # Make the tests conveniently executable
